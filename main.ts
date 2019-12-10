dad09f4c75a4bf5c3827513a7abaa802ea6517c5
Serkan KAMBER <58070215+serkankamber@users.noreply.github.com>
2019-12-10 22:26:52 +0300

    Add files via upload

--- main.ts
+++ main.ts
@@ -21,21 +21,25 @@ namespace sonar {
      */
     //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
     export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
-        // send pulse
-        pins.setPull(trig, PinPullMode.PullNone);
+        let d
         pins.digitalWritePin(trig, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(trig, 1);
-        control.waitMicros(10);
-        pins.digitalWritePin(trig, 0);
-
-        // read pulse
-        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
-
-        switch (unit) {
-            case PingUnit.Centimeters: return Math.idiv(d, 58);
-            case PingUnit.Inches: return Math.idiv(d, 148);
-            default: return d ;
+        if (pins.digitalReadPin(echo) == 0) {
+            pins.digitalWritePin(trig, 1);
+            pins.digitalWritePin(trig, 0);
+            d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+        } else {
+            pins.digitalWritePin(trig, 0);
+            pins.digitalWritePin(trig, 1);
+            d = pins.pulseIn(echo, PulseValue.Low, maxCmDistance * 58);
         }
+        let x = d / 39;
+        if (x <= 0 || x > 500) {
+            return 0;
+        }
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.round(x);
+            default: return Math.idiv(d, 2.54);
+        }
+
     }
 }
